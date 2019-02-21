8cc68bb0a80b7c9ed0c0a509438a512db181e788
Emiliano Leite <emiliano.leite@codus.com.br>
2019-02-21 13:13:31 -0300

    Add routing to App
    Co-authored-by: Guilherme Eiras <guialbertoeiras@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { ApolloProvider } from "react-apollo";
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import WipChallenges from "./components/WipChallenges";
 import client from "./graphql/client";
 
 export default class App extends React.Component {
   public render() { return (
-    <ApolloProvider client={client}>
-      <WipChallenges/>
-    </ApolloProvider>
+    <Router>
+      <ApolloProvider client={client}>
+        <Navbar />
+        <Route path="/challenges" component={WipChallenges}/>
+      </ApolloProvider>
+    </Router>
   ); }
 }
