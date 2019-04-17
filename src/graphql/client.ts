150884301382cdab987036b9afc05ca4bd11a2b6
Emiliano Leite <emiliano.leite@codus.com.br>
2019-04-17 08:30:13 -0300

    Prepare client to connect to remote server
    Co-authored-by: Guilherme Eiras <guialbertoeiras@hotmail.com>

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -1,10 +1,14 @@
 import { ApolloClient } from "apollo-boost";
 import { InMemoryCache } from "apollo-cache-inmemory";
+// import { createHttpLink } from "apollo-link-http";
 import { SchemaLink } from "apollo-link-schema";
 
 import schema from "./schema";
 
+const localSchema = new SchemaLink({ schema });
+// const remoteServer = createHttpLink({ uri: "http://localhost:4000" });
+
 export default new ApolloClient({
   cache: new InMemoryCache(),
-  link: new SchemaLink({ schema }),
+  link: localSchema
 });
