1bcc62d3bb688057a6551cda2a3473e1c9bf9e35
Emiliano Leite <emiliano.leite@codus.com.br>
2019-04-27 15:26:56 -0300

    feat(styles): Add global styles using emotion instead of *.css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,25 @@
-
+import { injectGlobal } from "emotion";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
 import configureStore from "./redux/ConfigureStore";
 import * as serviceWorker from "./serviceWorker";
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
 
 const store = configureStore();
 
