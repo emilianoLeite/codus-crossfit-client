89dd51811503ae3b32606086877c2fc3cae8fc04
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-05-19 18:00:56 -0300

    feat(style): using ant menu on header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Menu, Icon } from "antd";
+import { ClickParam } from "antd/lib/menu";
+import { withRouter, RouteComponentProps } from "react-router";
 
-export default () => {
+interface IProps extends RouteComponentProps {
+  rightContent: React.ReactNode;
+}
+
+const Navbar = ({ history, location, rightContent }: IProps) => {
+  const handleClick = (e: ClickParam) => {
+    history.push(`/${e.keyPath}`);
+  };
+  const selectedKeys = [
+    location.pathname.replace("/", "")
+  ];
+  if (location.pathname === "/") {
+    selectedKeys.push("wip_challenges");
+  }
+
   return (
-    <nav>
-      <Link to={"/challenges"}> Challenges </Link>
-      <Link to={"/wip_challenges"}> WIP Challenges </Link>
-    </nav>
+    <Menu onClick={handleClick} selectedKeys={selectedKeys} mode="horizontal">
+      <Menu.Item key="challenges" >
+        <Icon type="appstore" />
+        Challenges
+      </Menu.Item>
+      <Menu.Item key="wip_challenges">
+        <Icon type="project" />
+        WIP Challenges
+      </Menu.Item>
+
+      { rightContent && <div style={{ float: "right", marginRight: "15px" }}>
+        { rightContent }
+      </div> }
+    </Menu>
   );
 };
+
+export default withRouter(Navbar);
