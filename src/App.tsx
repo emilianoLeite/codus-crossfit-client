dc4c2f693c1b53c864068374ea24347734ed5b30
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-08-09 18:08:49 -0300

    refactor(App): transform into FunctionComponent

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,18 @@ import {
 } from "./pages/public";
 import { history } from "./redux/ConfigureStore";
 
-export default class App extends React.Component<{ store: Store }, {}> {
-  public render() { return (
-    <ReduxProvider store={this.props.store}>
-      <ConnectedRouter history={history}>
-        <ApolloProvider client={client(this.props.store)}>
-          <Header />
-          <Route exact path="/" component={WipChallengesPage} />
-          <Route path="/wip_challenges" component={WipChallengesPage} />
-          <Route path="/login" component={LoginPage} />
-          <PrivateRoutesRegistry/>
-        </ApolloProvider>
-      </ConnectedRouter>
-    </ReduxProvider>
-  ); }
-}
+const App: React.FunctionComponent<{ store: Store }> = ({ store }) =>  (
+  <ReduxProvider store={store}>
+    <ConnectedRouter history={history}>
+      <ApolloProvider client={client(store)}>
+        <Header />
+        <Route exact path="/" component={WipChallengesPage} />
+        <Route path="/wip_challenges" component={WipChallengesPage} />
+        <Route path="/login" component={LoginPage} />
+        <PrivateRoutesRegistry/>
+      </ApolloProvider>
+    </ConnectedRouter>
+  </ReduxProvider>
+);
+
+export default App;
