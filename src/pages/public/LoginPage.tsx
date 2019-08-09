9b9203a548eb7312ea5a0c861c1f3e8ccf159b7a
Guilherme Eiras <guialbertoeiras@hotmail.com>
2019-08-09 18:16:06 -0300

    refactor(LoginPage): transform into FunctionComponent

--- src/pages/public/LoginPage.tsx
+++ src/pages/public/LoginPage.tsx
@@ -12,53 +12,31 @@ import { IReduxAuthenticationProps } from "../../redux/reducers/AuthenticationRe
 
 interface IProps extends Redirectable.IRedirectableProps, IReduxAuthenticationProps { }
 
-interface IState {
-  from: object;
-  redirectToReferrer: boolean;
-}
-
-class LoginPage extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.submitLogin = this.submitLogin.bind(this);
-  }
-
-  public render() {
-    const LOGIN = gql`
-      mutation Login($email: String!, $password: String!) {
-        signIn(email: $email, password: $password) {
-          jwt
-          user {
-            id
-            email
-          }
-        }
+const LOGIN = gql`
+  mutation Login($email: String!, $password: String!) {
+    signIn(email: $email, password: $password) {
+      jwt
+      user {
+        id
+        email
       }
-    `;
-
-    const graphQLErrorMessages = (error: ApolloError) => {
-      return error.graphQLErrors.map((graphQLError) => {
-        return (
-          <div key={graphQLError.message}>
-            {graphQLError.message}
-          </div>
-        );
-      });
-    };
-
-    return (
-      <Mutation mutation={LOGIN}>
-        {(login: MutationFn, { error }: MutationResult) => (
-          <div>
-            {error && graphQLErrorMessages(error)}
-            <LoginForm onSubmit={this.submitLogin(login)} />
-          </div>
-        )}
-      </Mutation>
-    );
+    }
   }
+`;
 
-  private submitLogin(loginMutation: MutationFn) {
+const graphQLErrorMessages = (error: ApolloError) => {
+  return error.graphQLErrors.map((graphQLError) => {
+    return (
+      <div key={graphQLError.message}>
+        {graphQLError.message}
+      </div>
+    );
+  });
+};
+
+
+const LoginPage: React.FunctionComponent<IProps> = (props) => {
+  const submitLogin = (loginMutation: MutationFn) => {
     return async ({ email, password }: ILoginForm) => {
       const response = await loginMutation({
         variables: { email, password },
@@ -66,12 +44,23 @@ class LoginPage extends React.Component<IProps, IState> {
 
       const { user, jwt } = response && response.data.signIn;
 
-      this.props.authenticate();
-      this.props.setCurrentUser({ ...user, jwt });
-      this.props.redirect();
+      props.authenticate();
+      props.setCurrentUser({ ...user, jwt });
+      props.redirect();
     };
-  }
-}
+  };
+
+  return (
+    <Mutation mutation={LOGIN}>
+      {(login: MutationFn, { error }: MutationResult) => (
+        <div>
+          {error && graphQLErrorMessages(error)}
+          <LoginForm onSubmit={submitLogin(login)} />
+        </div>
+      )}
+    </Mutation>
+  );
+};
 
 export default compose<React.ComponentType<IProps>>(
   connect(null, { authenticate, setCurrentUser }),
